c76b90a795f0d1cd41cd4125d759e13aae1d58e2
Rohit Solia <rohit.solia@gmail.com>
2018-09-01 13:12:13 -0400

    Metronome off-chain validator, WIP

---
+++ index.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const _ = require('lodash')
+const fs = require('fs')
+const process = require('process')
+const Validator = require('./lib/validator')
+const Listener = require('./lib/listener')
+
+function init () {
+  program
+    .command('init-config')
+    .description('create in current directory configuration file for validator')
+  //    .option('--path [file_path]', 'path where sample config file should be generated')
+    .action(writeSampleConfigFile)
+
+  program
+    .command('launch')
+    .description('Launch metronome off chain validator to listen the import-export events and validate hash')
+    .action(launchValidator)
+
+  program
+    .parse(process.argv)
+
+  program.args.length < 1 &&
+        program.help()
+}
+
+function launchValidator () {
+  console.log('I am in launchValidator function')
+  const configStr = readConfig()
+  Listener.listen(configStr) // TODO: pass abiStr
+}
+
+function writeSampleConfigFile () {
+  console.log('writing sample config file')
+  const configPath = 'config.json'
+  const sampleConfig = [
+    '{                                              ',
+    '    "extends": "default",                      ',
+    '    "ETH": {                                   ',
+    '       "nodeUrl": "http://localhost:8545",     ',
+    '        "address": "0x0",                      ',
+    '        "password": "password1"                ',
+    '    },                                         ',
+    '    "ETC": {                                   ',
+    '       "nodeUrl": "http://localhost:8555",     ',
+    '        "address": "0x0",                      ',
+    '        "password": "password1"                ',
+    '    }                                          ',
+    '}                                              '
+  ]
+
+  if (!fs.existsSync(configPath)) {
+    fs.writeFileSync(configPath, sampleConfig.join('\n'))
+
+    console.log('Sample Configuration file created!')
+  } else {
+    console.log('Configuration file already exists')
+  }
+}
+
+const readConfig = _.memoize(function () {
+  return fs.readFileSync('config.json').toString()
+})
+
+init()
