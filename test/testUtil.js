5b8d53b99a8f3515046eb06591977ad5ec3070a3
manoj patidar <patidarmanoj@gmail.com>
2018-11-12 11:10:15 -0800

    fixed null value and big number issue

--- test/testUtil.js
+++ test/testUtil.js
@@ -83,7 +83,7 @@ async function getMET (chain, recepient) {
     })
     waitForTx(tx, web3.eth)
     metBalance = chain.contracts.metToken.balanceOf(web3.eth.accounts[0])
-    metBalance = ethers.utils.bigNumberify(metBalance.valueOf())
+    metBalance = ethers.utils.bigNumberify(web3.toHex(metBalance))
     if (metBalance.lt(ethers.utils.bigNumberify('10000000000000000'))) {
       // Buy more met from AC
       tx = await chain.contracts.autonomousConverter.convertEthToMet(1, { from: web3.eth.accounts[0], value: 1e18 })
