8709d76cbf5912cd47424890e8af9768ba006b16
Manoj <patidarmanoj@gmail.com>
2019-10-15 22:16:40 -0700

    qtum wallet fixes after fork, proposal js, other fixes

--- test/auctions.js
+++ test/auctions.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert
 const util = require('./testUtil')
 require('dotenv').config()
 
-var qChain, chains
+var qChain, chains, wallet
 
 before(async () => {
   chains = await util.initContracts()
@@ -18,16 +18,20 @@ describe('Auction test in qtum', () => {
 
   it('Should be able to buy from Auction', () => {
     return new Promise(async (resolve, reject) => {
-      var mintableBefore = await qChain.call(qChain.contracts.auctions, 'mintable', [])
-      assert(mintableBefore > 0, 'Auction has no mintable. Cants run this test case')
-      let hexAddress = await qChain.qtum.rawCall('gethexaddress', [process.env.qtum_validator_address])
-      let metBalanceBefore = await qChain.call(qChain.contracts.metToken, 'balanceOf', [hexAddress])
+      let hexAddress = await qChain.qtum.rawCall('gethexaddress', ['qY3Dc6fwnStAfW1gHKLjmD56uwizq58LHQ'])
+      console.log('qChain.contracts.METToken.info.address', qChain.contracts.METToken.info.address)
+      let fromHex = await qChain.qtum.rawCall('fromhexaddress', [qChain.contracts.METToken.info.address])
+      console.log('fromHex', fromHex)
+      let metBalanceBefore = await qChain.call(qChain.contracts.METToken, 'transfer', [hexAddress, 1])
       console.log('metBalanceBefore', metBalanceBefore.toString())
-      let tx = await qChain.qtum.rawCall('sendtocontract', [qChain.contracts.auctions.info.address, '00', 10, 2500000, 0.000001, from])
+      var mintableBefore = await qChain.call(qChain.contracts.Auctions, 'mintable', [])
+      console.log('mintableBefore', mintableBefore.toString())
+      assert(mintableBefore > 0, 'Auction has no mintable. Cants run this test case')
+      let tx = await qChain.qtum.rawCall('sendtocontract', [qChain.contracts.Auctions.info.address, '00', 10, 2500000, 0.000001, 'qY3Dc6fwnStAfW1gHKLjmD56uwizq58LHQ'])
       console.log('tx', tx)
-      var mintableAfter = await qChain.call(qChain.contracts.auctions, 'mintable', [])
+      var mintableAfter = await qChain.call(qChain.contracts.Auctions, 'mintable', [])
       console.log('mintableAfter', mintableAfter.toString())
-      var metBalanceAfter = await qChain.call(qChain.contracts.metToken, 'balanceOf', [hexAddress])
+      var metBalanceAfter = await qChain.call(qChain.contracts.METToken, 'balanceOf', [hexAddress])
       console.log('metBalanceAfter', metBalanceAfter.toString())
       assert(metBalanceAfter > metBalanceBefore, 'Failed buy in auction. MET balance wrong')
       assert(mintableBefore > mintableAfter, 'Failed buy in auction. Mintable remains same')
